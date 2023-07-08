e1d8f1ef196b89b6dc156437d4e559f352b4dcca
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2023-07-08 20:42:20 +0900

    abc308

--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -1,4 +1,4 @@
-use proconio::{input, fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
@@ -6,8 +6,6 @@ fn main() {
         n: i32,
         ab: [(f64, f64); n],
     }
-
-    
 
     let mut ans = vec![];
 
@@ -17,11 +15,10 @@ fn main() {
         i += 1;
     }
 
-
     // sort ans up by 1
-    ans.sort_by(|a, b| a.1.cmp(&b.0));
+    // ans.sort_by(|a, b| a.1.cmp(&b.0));
 
     for (a, i) in ans {
-        print!("{} {}\n",a, i);
+        print!("{} {}\n", a, i);
     }
 }
