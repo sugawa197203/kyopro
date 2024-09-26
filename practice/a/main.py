96de7d0d9ceb4714f6ef83b9783cbd8def49e97a
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-09-26 10:57:13 +0000

    ractice

---
+++ practice/a/main.py
@@ -0,0 +1,8 @@
+import numpy as np; import sortedcontainers as sc; import math; import bisect; import itertools; import sys; debug = lambda *arg : print(*arg, file=sys.stderr);
+
+a = int(input())
+b, c = map(int, input().split())
+s = input()
+
+print(a+b+c, s)
+
