2d1c16f7ed01fb4185b62e59483a6fefe57bbdba
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-09-29 02:20:09 +0000

    runnner

--- runner.py
+++ runner.py
@@ -2,6 +2,12 @@
 import os
 import subprocess
 
+# check acc and oj login
+result = subprocess.run(["acc", "session"])
+if result.returncode != 0:
+    print("Please login to acc")
+    exit(1)
+
 contest = input("input contest name>")
 
 if not os.path.exists(contest):
@@ -18,20 +24,37 @@ while True:
         print("bye")
         break
 
+    if not os.path.exists("test"):
+        result = subprocess.run(["oj", "d", f"https://atcoder.jp/contests/{contest}/tasks/{contest}_{submittion[0]}"])
+        if result.returncode != 0:
+            print("Error downloading problem")
+            exit(1)
+        print("Downloaded problem")
+
     problem = submittion[0]
     lang = "py"
     if len(submittion) == 2:
         lang = submittion[1]
-    
+
     os.chdir(problem)
+    print("Testing...")
+    
+    result = subprocess.run(["oj", "t", "-c", f"python main.py"])
+    if result.returncode != 0:
+        print("!!! Error testing !!!")
+        continue
     langcode = {
         "py": "5078",
         "cpp": "5028",
     }[lang]
-    stdinput = "pra1"
+    stdinput = "y"
+
+    print("Submitting...")
     
     result = subprocess.run(["acc", "s", "--", f"main.{lang}", "--language", langcode], input=stdinput.encode())
     if result.returncode != 0:
         print("Error submitting")
         exit(1)
+    print("### Submitted ###")
+
     os.chdir("..")
