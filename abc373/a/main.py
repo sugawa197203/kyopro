ca9f91c062955685b4d7b664cd5d4e9b84775677
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-09-29 02:20:01 +0000

    abc373

---
+++ abc373/a/main.py
@@ -0,0 +1,11 @@
+import numpy as np; import sortedcontainers as sc; import math; import bisect; import itertools; import sys; debug = lambda *arg : print(*arg, file=sys.stderr);
+
+
+
+s = 0
+for i in range(12):
+    if len(input()) == i + 1:
+        s += 1
+
+print(s)
+
---
+++ abc373/b/main.py
@@ -0,0 +1,4 @@
+import numpy as np; import sortedcontainers as sc; import math; import bisect; import itertools; import sys; debug = lambda *arg : print(*arg, file=sys.stderr);
+
+
+
