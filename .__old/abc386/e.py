74c6e9b27edf5444c39049549d81658a3f4b9a1d
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-12-29 15:04:33 +0900

    abc386

--- .__old/abc386/e.py
+++ .__old/abc386/e.py
@@ -1,1 +1,15 @@
+from itertools import combinations
+from functools import reduce
+def xor(__a:list)->int:return reduce(lambda __x, __y: __x ^ __y, __a) if len(__a) > 1 else 0 if len(__a) == 0 else __a[0]
 
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = 0
+allxor = 0
+if N - K < K:
+	K = N - K
+	allxor = xor(A)
+
+[ans := max(ans, allxor ^ xor(a)) for a in combinations(A, K)]
+print(ans)
