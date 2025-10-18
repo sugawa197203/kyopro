51ddec6a0dd067f49060bc1c18d61e546fa22dac
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2025-10-18 20:37:17 +0900

    template

---
+++ init.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM 引数は2つ以上指定されているか？
+if "%1"=="" (
+	echo Usage: %0 filename
+	exit /b
+)
+
+if "%1"=="abc" (
+	if "%2"=="" (
+		echo Usage: %0 filename
+		exit /b
+	)
+
+	mkdir abc%2
+	copy template\abc\python\* abc%2
+	chdir abc%2
+    uv init .
+    uv sync
+	exit /b
+)
