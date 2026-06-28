98e280193254f7e9e14c95e60d69f4b7e540f1c4
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2026-06-28 22:05:03 +0900

    init

--- init.bat
+++ init.bat
@@ -12,7 +12,7 @@ if "%1"=="abc" (
 		exit /b
 	)
 
-	mkdir abc%2
+	mkdir -p abc%2
 	copy template\abc\python\* abc%2
 	chdir abc%2
     uv init .
@@ -26,7 +26,7 @@ if "%1"=="abc" (
 		exit /b
 	)
 
-	mkdir %2
+	mkdir -p %2
 	copy template\other\* %2
 	chdir %2
 	uv init .
@@ -34,4 +34,19 @@ if "%1"=="abc" (
 	uv sync
 	cmd
 	exit /b
+)else if "%1"=="adt" (
+	if "%2"=="" (
+		echo Usage: %0 filename
+		exit /b
+	)
+
+	mkdir -p adt\%2
+	copy template\other\* adt\%2
+	chdir adt\%2
+	uv init .
+	uv add numpy sortedcontainers git+https://github.com/not522/ac-library-python
+	uv sync
+	cmd
+	exit /b
 )
+
